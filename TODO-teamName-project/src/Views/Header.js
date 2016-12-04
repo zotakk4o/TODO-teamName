4085cfdbdf59d733e0d513db0c550f15e0b686c4
Petar Staynov <petar.staynov@gmail.com>
2016-12-05 01:19:44 +0200

    CSS test and some cleanup

--- TODO-teamName-project/src/Views/Header.js
+++ TODO-teamName-project/src/Views/Header.js
@@ -4,7 +4,7 @@ import './header.css';
 export default class Header extends Component {
     render() {
         return (
-            <div className="jumbotron">
+            <div className="header">
                 <h1>Dreboliiobmenqlnik</h1>
                 <div>{this.props.children}</div>
             </div>
