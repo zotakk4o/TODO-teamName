62e6439fceea447c9aec9089d9574abd5ea40190
hristo2612 <hristoapps@gmail.com>
2016-12-05 19:14:01 +0200

    Added aboutView to controller

--- TODO-teamName-project/src/Controllers/AboutPage.js
+++ TODO-teamName-project/src/Controllers/AboutPage.js
@@ -4,10 +4,7 @@ import AboutView from '../Views/AboutView';
 export default class AboutPage extends Component {
     render() {
         return (
-            <div classID="about-view">
-                <h1>About</h1>
-                <p>This is a simple advertisement website.</p>
-            </div>
+            <AboutView />
         );
     }
 }
