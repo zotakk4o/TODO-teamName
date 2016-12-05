8dfdf92287a32f7e3b9523fd1c9c92af341eeca6
hristo2612 <hristoapps@gmail.com>
2016-12-05 19:39:56 +0200

    Add files via upload

--- TODO-teamName-project/src/Views/AboutView.js
+++ TODO-teamName-project/src/Views/AboutView.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 
 export default class AboutView extends Component {
     render() {
+        let style = {
+          padding: 20,
+        };
         return (
-            <div classID="about-view">
-                <h1>About</h1>
-                <p>This is a simple advertisement website.</p>
+            <div classID="about-view" style={style}>
+                <div className="panel panel-default" >
+                    <div className="panel-heading"><h1>About</h1></div>
+                    <div className="panel-body"><p>This is a simple advertisement website. You can post your adverts in the Advertisements tab (but first log in/register). Also if you don't like for some reason your post you can edit it or directly delete it!</p></div>
+                </div>
             </div>
         );
     }
