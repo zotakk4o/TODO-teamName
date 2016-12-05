41027076874a27f90cdccc8eaa8bac0a6bfab12c
hristo2612 <hristoapps@gmail.com>
2016-12-05 20:12:59 +0200

    About View

--- TODO-teamName-project/src/Views/AboutView.js
+++ TODO-teamName-project/src/Views/AboutView.js
@@ -3,14 +3,22 @@ import React, { Component } from 'react';
 export default class AboutView extends Component {
     render() {
         let style = {
+
           padding: 20,
+
         };
         return (
             <div classID="about-view" style={style}>
+
                 <div className="panel panel-default" >
-                    <div className="panel-heading"><h1>About</h1></div>
-                    <div className="panel-body"><p>This is a simple advertisement website. You can post your adverts in the Advertisements tab (but first log in/register). Also if you don't like for some reason your post you can edit it or directly delete it!</p></div>
+
+                    <div className="panel-heading"><h1>{this.props.title}</h1></div>
+                    <div className="panel-body"><p>{this.props.overview}</p></div>
+                    <div className="panel-heading"><h2>Contributors</h2></div>
+                    <div className="panel-body"><p>{this.props.contributors.join(', ')}</p></div>
+
                 </div>
+
             </div>
         );
     }
